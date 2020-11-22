b8a9e5356fe7b96c3d4bf0209f1e0bb79880783d
matee98 <matzab37@gmail.com>
2020-11-22 21:30:09 +0100

    commands Bindings done

---
+++ ViewModel/MenuViewModel.cs
@@ -0,0 +1,54 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ViewModel
+{
+    public class MenuViewModel : ViewModelBase
+    {
+        public MenuViewModel()
+        {
+            ExitCommand = new RelayCommand(ExitCommandExecute, () => true);
+            OpenCommand = new RelayCommand(OpenCommandExecute, () => true);
+            CloseCommand = new RelayCommand(CloseCommandExecute, () => true);
+        }
+
+        public RelayCommand ExitCommand
+        {
+            get;
+            private set;
+        }
+
+        public void ExitCommandExecute()
+        {
+            Environment.Exit(0);
+        }
+
+        public RelayCommand OpenCommand
+        {
+            get;
+            private set;
+        }
+
+        public void OpenCommandExecute()
+        {
+            
+        }
+
+        public RelayCommand CloseCommand
+        {
+            get;
+            private set;
+        }
+
+        public void CloseCommandExecute()
+        {
+            
+        }
+    }
+}
