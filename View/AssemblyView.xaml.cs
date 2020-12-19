1480631b5b8bb34c7eef1c1265fb3557a4c2820c
SitamMatt <mateuszszewc@op.pl>
2020-12-19 02:24:24 +0100

    Tabitem displays assembly view

---
+++ View/AssemblyView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ViewModel;
+
+namespace View
+{
+    public partial class AssemblyView : UserControl
+    {
+        public AssemblyView()
+        {
+            InitializeComponent();
+        }
+        
+        private void ItemExpanded(object sender, RoutedEventArgs e)
+        {
+            var item = e.OriginalSource as TreeViewItem;
+            var param = (item.DataContext as TreeNode);
+            var vm = DataContext as MainViewModel;
+            if (vm?.ExpandCommand.CanExecute(param) ?? false)
+            {
+                vm.ExpandCommand.Execute(param);
+            }
+        }
+    }
+}
