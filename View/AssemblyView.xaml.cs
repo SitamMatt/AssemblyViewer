62079f48e5fa800f2456c961dc56425a395c8286
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-01 21:53:31 +0100

    ExtendedTreeView implementation

--- View/AssemblyView.xaml.cs
+++ View/AssemblyView.xaml.cs
@@ -12,18 +12,5 @@ namespace View
         {
             InitializeComponent();
         }
-        
-        
-        public AssemblyInfo Asm { get; set; }
-        private void ItemExpanded(object sender, RoutedEventArgs e)
-        {
-            var item = e.OriginalSource as TreeViewItem;
-            var param = (item.DataContext as TreeNode);
-            var vm = DataContext as MainViewModel;
-            if (vm?.ExpandCommand.CanExecute(param) ?? false)
-            {
-                vm.ExpandCommand.Execute(param);
-            }
-        }
     }
 }
---
+++ View/Converters/AssemblyTreeItemConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace View.Converters
+{
+    public class AssemblyTreeItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var args = value as RoutedEventArgs;
+            var node = args.OriginalSource as TreeViewItem;
+            return node.DataContext;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
