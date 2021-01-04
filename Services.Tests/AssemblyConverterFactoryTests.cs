be496c2bbd4d965932630ed77b492dd5eadd24ae
matee98 <matzab37@gmail.com>
2021-01-04 21:14:48 +0100

    AssemblyConverterFactory tests

---
+++ Services.Tests/AssemblyConverterFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Tests
+{
+    class AssemblyConverterFactoryTests
+    {
+        [Test]
+        public void CreateTest()
+        {
+            var converter = new Mock<AssemblyConverter>();
+            var factory = new Mock<IAssemblyConverterFactory>();
+            factory.Setup(x => x.Create()).Returns(converter.Object);
+            Assert.IsInstanceOf(typeof(AssemblyConverter), factory.Object.Create());
+        }
+    }
+}
